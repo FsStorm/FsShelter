62781920874568a5b73ea797626b716dcb6cac09
eugene.tolmachev <eugene.tolmachev@prolucid.ca>
2015-08-12 13:46:21 -0400

    basic docs

---
+++ docs/content/guaranteed.fsx
@@ -0,0 +1,115 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use 
+// it to define helpers that you do not want to show in the documentation.
+#I "../../bin"
+#r "FsStorm.dll"
+#r "FsJson.dll"
+#r "FsLogging.dll"
+
+open FsJson
+open Storm
+
+(**
+Defining reliable spouts
+========================
+[Processing guarantees](https://storm.apache.org/documentation/Guaranteeing-message-processing.html) are the biggest selling point of Storm, please see the official docs for the details.
+FsStorm implements reliability semantics with "housekeeper" functions: defaultHousekeeper can be used as is for transient sources or as an inspiration for reliability over external/persistent sources. 
+The spout implementation is fairly similar to the "unreliable" version, with the addition of unique (int64) tuple ID:
+
+*)
+let rnd = new System.Random() // used for generating random messages
+
+///cfg: the configution passed in by storm
+///runner: a spout runner function (passed in from topology)
+let spout runner (cfg:Configuration) = 
+    let count = ref 0L
+    //define the "next" function
+    //emit: a function that emits message to storm with unique ID
+    let next emit = 
+         fun () -> async { 
+                     tuple [ rnd.Next(0, 100) ] 
+                     |> emit (Threading.Interlocked.Increment &count.contents) 
+                   }
+    //run the spout
+    next |> runner
+	
+(**
+Anchoring and named streams
+========================
+
+FsStorm has helper functions to emit to a named stream or to anchor a tuple:
+
+*)
+///cfg: the configuration passed in from Storm
+///runner: passed in from topology
+///emit: passed in from topology
+let addOneBolt runner emit cfg = 
+    //define the consumer function 
+    let add (msg : Json) =
+        async { 
+            let x = msg?tuple.[0].ValI + 1
+            tuple [ x ]
+            // write to a named stream
+            |> namedStream (match x % 2 with | 0 -> "even" | _ -> "odd")
+            // anchor to ensure the entire tuple tree is processed before the spout is ack'ed
+            |> anchor msg // anchor to the original message
+            |> emit
+        }
+    //run the bolt
+    add |> runner
+
+(**
+Example of parametrization and use of the tuple's origin (component that emitted it and the stream that it arrived on) inspection:
+
+*)
+///cfg: the configuration passed in by Storm
+///runner: passed in from topology
+///log: log write
+let resultBolt runner log (cfg:Configuration) = 
+    let desc = cfg.Json?conf?desc.Val // the value passed in with the submitted topology Config
+    //define the function that will return the consumer 
+    let logResult (msg : Json) = 
+          async { 
+            log desc (sprintf "origin: %A(%A), data: %A" msg?comp.Val msg?stream.Val msg?tuple.[0].ValI)
+          }
+    //run the bolt
+    logResult |> runner
+
+(**
+Topology with named streams and config overrides
+========================
+
+*)
+
+open StormDSL
+
+let topology = 
+    { TopologyName = "FstGuaranteed"
+      Spouts = 
+          [ { Id = "ReliableSpout"
+              Outputs = [ Default [ "number" ] ]
+              Spout = Local { Func = spout (Storm.reliableSpoutRunner Storm.defaultHousekeeper) }
+              Config = jval [ "topology.max.spout.pending", jval 123 ] // override "backpressure"
+              Parallelism = 1 } ]
+      Bolts = 
+          [ { Id = "AddOneBolt"
+              Outputs = [ Named("even", [ "number" ]) // named stream "even"
+                          Named("odd", [ "number" ]) ]
+              Inputs = [ DefaultStream "ReliableSpout", Shuffle ]
+              Bolt = Local { Func = addOneBolt Storm.autoAckBoltRunner Storm.emit }
+              Config = JsonNull
+              Parallelism = 2 }
+            { Id = "EvenResultBolt"
+              Outputs = []
+              Inputs = [ Stream("AddOneBolt","even"), Shuffle ]
+              Bolt = Local { Func = resultBolt Storm.autoAckBoltRunner }
+              Config = jval ["desc", "even"] // pass custom config property to the component
+              Parallelism = 1 }
+            { Id = "OddResultBolt"
+              Outputs = []
+              Inputs = [ Stream("AddOneBolt","odd"), Shuffle ]
+              // logs to custom (FsLogging) pid-based log file
+              Bolt = Local { Func = resultBolt Storm.autoAckBoltRunner Logging.log }
+              Config = jval ["desc", "odd"] // pass custom config property to the component
+              Parallelism = 1 } ] }
+
--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -2,51 +2,127 @@
 // This block of code is omitted in the generated HTML documentation. Use 
 // it to define helpers that you do not want to show in the documentation.
 #I "../../bin"
+#r "FsJson.dll"
+#r "Storm.dll"
 
 (**
-FSharp.ProjectScaffold
+FsStorm
 ======================
 
-Documentation
+Overview
+-------
+FsStorm is a library for implementation of [Apache Storm](https://storm.apache.org/) components, definition of topologies in F# DSL and submission via F# scripts for execution.
+The topology and the components could be implemented in a single EXE project and are executed by Storm via its [multilang](https://storm.apache.org/documentation/Multilang-protocol.html) protocol as separate processes - one for each task/instance.
+Accompanying FsJson library is used for dealing with Json structures passed in and out of Storm.
+
+FsStorm components
+-----------------------
+
+FsStorm components are defined as functions that take at least one (last) argument: configuration passed in from Storm. In practice, you'll want to pass all your dependencies in, and that means at least one other: a runner, passed in from your topology. 
+Additionally you can pass as many arguments from the topology as needed.
+Think of the component function as "main" for your program. The "main" function will be called by FsStorm once per instance of every component and its purpose is to construct either "next" function for spouts or "consume" function for bolts and pass it to a runner.
+FsStorm implements several runners that either talk to Storm or allow you to unit-test your components by recording outputs or playing back the inputs.
+
+
+FsStorm tuples
+-----------------------
+
+Storm components communicate by passing tuples to each other over streams. The tuples are emmited into streams and have schema defined by the spout topology Output element.
+Storm multilang is wrapped and accessible via included FsJson.
+In addition to raw json access, FsStorm defines several helpers: tuple, namedStream, anchor, etc. that help to abstract the specifics of underlying multilang.
+
+
+Example of a spout
+-----------------------
+
+*)
+open FsJson
+open Storm
+
+let rnd = new System.Random() // used for generating random messages
+
+///spout - produces messages
+///cfg: the configuration passed in by storm
+///runner: a spout runner function (passed in from topology)
+let spout runner cfg = 
+    //define the function that will return the emitter function
+    //emit: a function that emits message to storm (passed in by the runner)
+    let createEmitter emit = fun () -> async { tuple [ rnd.Next(0, 100) ] |> emit } //the "next" function
+    //run the spout
+    createEmitter |> runner
+
+(**
+Topology DSL in F#
+-----------------------
+*)
+
+//define the storm topology
+open StormDSL
+open FsJson
+
+//example of using FsStorm DSL for defining topologies
+let topology = 
+    { TopologyName = "FstSample"
+      Spouts = 
+          [ { Id = "SimpleSpout" // unique Id
+              Outputs = [ Default [ "number" ] ] // default stream schema
+              Spout = Local { Func = spout Storm.simpleSpoutRunner }
+              // configuration Storm will use with each instance of the component 
+              Config = JsonNull 
+              Parallelism = 1 } ] // one instance
+      Bolts = 
+          [ { Id = "AddOneBolt"
+              Outputs = [ Default [ "number" ] ]
+              // default stream of SimpleSpout, no grouping/affinity 
+              Inputs = [ DefaultStream "SimpleSpout", Shuffle ]
+              Bolt = Local { Func = addOneBolt Storm.autoAckBoltRunner Logging.log Storm.emit}
+              Config = JsonNull
+              Parallelism = 2 } // two instances of the process/component
+            { Id = "ResultBolt"
+              Outputs = [] // no output
+              Inputs = [ DefaultStream "AddOneBolt", Shuffle ]
+              Bolt = Local { Func = resultBolt Storm.autoAckBoltRunner Logging.log }
+              Config = JsonNull
+              Parallelism = 2 } ] }
+
+(**
+Submitting the topology using F# scripts
+-----------------------
+*)
+
+#I "../../Refs"
+#load "StormSubmit.fsx"
+
+let binDir = "build"
+
+StormSubmit.runTopology binDir "localhost" StormSubmit.default_nimbus_port
+
+
+(**
+Samples & documentation
+-----------------------
+
+ * [FstSample](sample.html) contains a "unreliable" spout example - emitted tuples do not require ack, could be lost in case of failure.
+
+ * [FstGuaranteedSample](guaranteed.html) contains a "reliable" spout example - emitted tuples have unique ID and require ack.
+
+ * [API Reference](reference/index.html) contains automatically generated documentation for public types, modules
+   and functions in the library. 
+ 
+Getting FsStorm
+----------------
 
 <div class="row">
   <div class="span1"></div>
   <div class="span6">
     <div class="well well-small" id="nuget">
-      The FSharp.ProjectScaffold library can be <a href="https://nuget.org/packages/FSharp.ProjectScaffold">installed from NuGet</a>:
-      <pre>PM> Install-Package FSharp.ProjectScaffold</pre>
+      The FsStorm library can be <a href="https://nuget.org/packages/FsStorm">installed from NuGet</a>:
+      <pre>PM> Install-Package FsStorm</pre>
     </div>
   </div>
   <div class="span1"></div>
 </div>
 
-Example
--------
-
-This example demonstrates using a function defined in this sample library.
-
-*)
-#r "FSharp.ProjectScaffold.dll"
-open FSharp.ProjectScaffold
-
-printfn "hello = %i" <| Library.hello 0
-
-(**
-Some more info
-
-Samples & documentation
------------------------
-
-The library comes with comprehensible documentation. 
-It can include tutorials automatically generated from `*.fsx` files in [the content folder][content]. 
-The API reference is automatically generated from Markdown comments in the library implementation.
-
- * [Tutorial](tutorial.html) contains a further explanation of this sample library.
-
- * [API Reference](reference/index.html) contains automatically generated documentation for all types, modules
-   and functions in the library. This includes additional brief samples on using most of the
-   functions.
- 
 Contributing and copyright
 --------------------------
 
@@ -55,13 +131,13 @@ the project and submit pull requests. If you're adding a new public API, please 
 consider adding [samples][content] that can be turned into a documentation. You might
 also want to read the [library design notes][readme] to understand how it works.
 
-The library is available under Public Domain license, which allows modification and 
+The library is available under MIT license, which allows modification and 
 redistribution for both commercial and non-commercial purposes. For more information see the 
 [License file][license] in the GitHub repository. 
 
-  [content]: https://github.com/fsprojects/FSharp.ProjectScaffold/tree/master/docs/content
-  [gh]: https://github.com/fsprojects/FSharp.ProjectScaffold
-  [issues]: https://github.com/fsprojects/FSharp.ProjectScaffold/issues
-  [readme]: https://github.com/fsprojects/FSharp.ProjectScaffold/blob/master/README.md
-  [license]: https://github.com/fsprojects/FSharp.ProjectScaffold/blob/master/LICENSE.txt
+  [content]: https://github.com/et1975/FsStorm/tree/master/docs/content
+  [gh]: https://github.com/et1975/FsStorm
+  [issues]: https://github.com/et1975/FsStorm/issues
+  [readme]: https://github.com/et1975/FsStorm/blob/master/README.md
+  [license]: https://github.com/et1975/FsStorm/blob/master/LICENSE.txt
 *)
