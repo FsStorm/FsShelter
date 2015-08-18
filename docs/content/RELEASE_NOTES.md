19d5456a9de9d188f13b259930ac41ab185f96d6
eugene.tolmachev <eugene.tolmachev@prolucid.ca>
2015-08-18 10:24:23 -0400

    Bump version to 0.0.3-beta

--- docs/content/RELEASE_NOTES.md
+++ docs/content/RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+#### 0.0.3-beta - August 2015
+* Reverting FSharp.Core as a dependency
+
 #### 0.0.2-beta - August 2015
 * Externalizing Thrift dependency
 
--- src/FstSample/AssemblyInfo.fs
+++ src/FstSample/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FstSample")>]
 [<assembly: AssemblyProductAttribute("FsStorm")>]
 [<assembly: AssemblyDescriptionAttribute("F# DSL and runtime for Storm topologies")>]
-[<assembly: AssemblyVersionAttribute("0.0.2")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.2")>]
+[<assembly: AssemblyVersionAttribute("0.0.3")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.2"
+    let [<Literal>] Version = "0.0.3"
