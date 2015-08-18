beea1aa706ed75cb7fbdba4e901107934391d609
eugene.tolmachev <eugene.tolmachev@prolucid.ca>
2015-08-18 09:53:49 -0400

    Bump version to 0.0.2-beta

--- src/FstSampleTest/AssemblyInfo.fs
+++ src/FstSampleTest/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FstSampleTest")>]
 [<assembly: AssemblyProductAttribute("FsStorm")>]
 [<assembly: AssemblyDescriptionAttribute("F# DSL and runtime for Storm topologies")>]
-[<assembly: AssemblyVersionAttribute("0.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+[<assembly: AssemblyVersionAttribute("0.0.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.1"
+    let [<Literal>] Version = "0.0.2"
