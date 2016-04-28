7f0ec5e7c8e7cc9137fb63d4662864be2a425a63
eugene.tolmachev <eugene.tolmachev@prolucid.ca>
2016-04-28 15:03:35 -0400

    Bump version to 0.0.6

--- ext/StormThrift/Properties/AssemblyInfo.cs
+++ ext/StormThrift/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("StormThrift")]
 [assembly: AssemblyProductAttribute("FsShelter")]
 [assembly: AssemblyDescriptionAttribute("F# DSL and runtime for Storm topologies")]
-[assembly: AssemblyVersionAttribute("0.0.5")]
-[assembly: AssemblyFileVersionAttribute("0.0.5")]
+[assembly: AssemblyVersionAttribute("0.0.6")]
+[assembly: AssemblyFileVersionAttribute("0.0.6")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.0.5";
+        internal const string Version = "0.0.6";
     }
 }
